0e0d96526a39b8f0b0272ed64fa26f27e31c1346
Kelsey Chua <77289991+kelc4@users.noreply.github.com>
2022-08-12 12:57:57 -0700

    Text across, images shuffle successfully

--- src/App.js
+++ src/App.js
@@ -7,10 +7,28 @@ function App() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState([]);
+  const [reRender, setReRender] = useState(false);
 
   const shuffle = (array) => {
-    //recursive function
+    const length = array.length;
+    shuffleHelper(array, length);
+  }
 
+  const shuffleHelper = (array, num) => {
+    //base case
+    if(num <= 1) {
+      return array;
+    }
+
+    //Choose random index to swap with last element in shortened array
+    const randNum = Math.floor(Math.random() * num);
+    //alert(randNum);
+    const temp = array[num - 1];
+
+    array[num - 1] = array[randNum];
+    array[randNum] = temp;
+
+    return shuffleHelper(array, num - 1);
   }
 
   useEffect(() => {
@@ -39,18 +57,19 @@ function App() {
 
   const handleClick = () => {
     shuffle(data);
-    alert('clicked');
+    setReRender(!reRender);
   };
 
   return (
     <div className="App">
       <div className="Photos-area">
         {data.map((photo) => (
-          <article className = "Article">
+          <article className = "Article" key="{photo}">
             <img className="Image" src= {photo.url} alt={photo.id}/>
             <h1 className="Image-text">{photo.title}</h1>
           </article>
         ))}
+
       </div>
 
 
