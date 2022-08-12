0212c4035260e7eecdd1c83d18d867796b45dcbd
Kelsey Chua <77289991+kelc4@users.noreply.github.com>
2022-08-12 10:52:29 -0700

    Images display fetched data

--- src/App.js
+++ src/App.js
@@ -1,63 +1,62 @@
 //import logo from './logo.svg';
 import './App.css';
-//import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
 
-  // const [data, setData] = useState(null);
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState([]);
 
-  // useEffect(() => {
-  //   fetch('http://jsonplaceholder.typicode.com/photos')
-  //     .then(response => {
-  //       if (response.ok) {
-  //         return response.json()
-  //       }
-  //       throw response;
-  //     })
-  //     .then(data => {
-  //       setData(data);
-  //     })
-  //     .catch(error => {
-  //       console.error("Error fetching data: ", error);
-  //       setError(error);
-  //     })
-  //     .finally(() => {
-  //       setLoading(false);
-  //     })
-  // }, []);
+  const shuffle = (array) => {
+    //recursive function
 
-  // const photoArray = data.
+  }
+
+  useEffect(() => {
+    const fetchPhotos = () => {
+      fetch('http://jsonplaceholder.typicode.com/photos')
+        .then(response => {
+          if (response.ok) {
+            return response.json()
+          }
+          throw response;
+        })
+        .then(data => {
+          setData(data);
+        })
+        .catch(error => {
+          console.error("Error fetching data: ", error);
+          setError(error);
+        })
+        .finally(() => {
+          setLoading(false);
+      })
+    }
+
+    fetchPhotos();
+  }, []);
+
+  const handleClick = () => {
+    shuffle(data);
+    alert('clicked');
+  };
 
   return (
     <div className="App">
       <div className="Photos-area">
-        <article className = "Article">
-          <img className="Image" src= "https://via.placeholder.com/600/92c952" alt="background"/>
-          <h1 className="Image-text">AHHHHHHHHHH</h1>
-        </article>
-
-        <article className = "Article">
-          <img className="Image" src= "https://via.placeholder.com/600/92c952" alt="background"/>
-          <h1 className="Image-text">AHHHHHHHHHH</h1>
-        </article>
-
-        <article className = "Article">
-          <img className="Image" src= "https://via.placeholder.com/600/92c952" alt="background"/>
-          <h1 className="Image-text">AHHHHHHHHHH</h1>
-        </article>
-
-        <article className = "Article">
-          <img className="Image" src= "https://via.placeholder.com/600/92c952" alt="background"/>
-          <h1 className="Image-text">AHHHHHHHHHH</h1>
-        </article>
+        {data.map((photo) => (
+          <article className = "Article">
+            <img className="Image" src= {photo.url} alt={photo.id}/>
+            <h1 className="Image-text">{photo.title}</h1>
+          </article>
+        ))}
       </div>
 
-      
+
       <div className="Button-area">
-        <button>
-          <h1>Shuffle</h1>
+        <button className="Button" onClick={handleClick}>
+          <b>Shuffle</b>
         </button>
       </div>
     </div>
